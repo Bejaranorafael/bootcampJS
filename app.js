24b69789e59902a3008ca24a9893947e24019d4d
Rafael Bejarano <rbejarano577@gmail.com>
2023-07-13 23:01:30 -0300

    archivo lista- tarea 1

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+const informacion = {
+  nombre: 'Tu nombre',
+  edad: 25,
+  esDesarrollador: true,
+  fechaNacimiento: new Date('1998-01-01'),
+  libroFavorito: {
+    titulo: 'El libro favorito',
+    autor: 'Autor del libro',
+    fecha: new Date('2022-05-01'),
+    url: 'https://ejemplo.com/libro-favorito'
+  }
+};
+
+module.exports = informacion;
